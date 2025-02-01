963055d38add63d75abdd376cbb4b01bc1586170
Ismael <145430109+ismasarry@users.noreply.github.com>
2025-02-01 11:11:19 +0100

    conexion y asociaciones de modelos

--- back/models/associations.js
+++ back/models/associations.js
@@ -1,6 +1,9 @@
 import User from './user.js';
 import User_role from './user_role.js';
 import Roles from './role.js';
+import Grups from './grups.js';
+import Subject from './subject.js';
+import SubjectGrups from './subject_grups.js';
 
 User.hasMany(User_role, { 
   as: 'User_roles', 
@@ -22,4 +25,34 @@ Roles.hasMany(User_role, {
   foreignKey: 'rol_id' 
 });
 
-export { User_role, User, Roles };
+Grups.hasMany(SubjectGrups, {
+  as: 'SubjectGrups',
+  foreignKey: 'grup_id',
+});
+
+SubjectGrups.belongsTo(Grups, {
+  as: 'Grup',
+  foreignKey: 'grup_id',
+});
+
+Subject.hasMany(SubjectGrups, {
+  as: 'SubjectGrups',
+  foreignKey: 'subject_id',
+});
+
+SubjectGrups.belongsTo(Subject, {
+  as: 'Subject',
+  foreignKey: 'subject_id',
+});
+
+User.hasMany(SubjectGrups, {
+  as: 'SubjectGrups',
+  foreignKey: 'teacher_id',
+});
+
+SubjectGrups.belongsTo(User, {
+  as: 'Teacher',
+  foreignKey: 'teacher_id',
+});
+
+export { User_role, User, Roles, Grups, Subject, SubjectGrups };
